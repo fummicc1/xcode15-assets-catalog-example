59a3b641f47e269d9fc6335379de6e4ced05641b
fummicc1 <fumiya.tennis1@gmail.com>
2023-10-23 10:31:37 +0900

    Initial Commit

---
+++ Xcode15AssetsCatalogExample/Xcode15AssetsCatalogExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode15AssetsCatalogExampleApp.swift
+//  Xcode15AssetsCatalogExample
+//
+//  Created by Fumiya Tanaka on 2023/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Xcode15AssetsCatalogExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
