7524f699ca238697e8edc27a2b5143676c1e6b7b
fummicc1 <fumiya.tennis1@gmail.com>
2023-10-23 10:39:24 +0900

    color

--- Xcode15AssetsCatalogExample/ContentView.swift
+++ Xcode15AssetsCatalogExample/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack() {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.accent)
+                Text("Hello, world!")
+                    .foregroundStyle(Color.error)
+            }
         }
         .padding()
     }
